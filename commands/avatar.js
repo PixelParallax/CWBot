6527d39f1a12f5dea10e51453d254586bde45a04
PixelParallax <abombsc@gmail.com>
2021-09-11 23:07:06 -0400

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -6,8 +6,16 @@ module.exports = {
 		.setDescription('Get the avatar URL of the selected user, or your own avatar.')
 		.addUserOption(option => option.setName('target').setDescription('The user\'s avatar to show')),
 	async execute(interaction) {
-		const user = interaction.options.getUser('target');
-		if (user) return interaction.reply(`${user.username}'s avatar: ${user.displayAvatarURL({ dynamic: true })}`);
-		return interaction.reply(`Your avatar: ${interaction.user.displayAvatarURL({ dynamic: true })}`);
+		if (interaction.commandName === 'avatar') {
+			if (member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES, true)) {
+				const user = interaction.options.getUser('target');
+				if (user) return interaction.reply(`${user.username}'s avatar: ${user.displayAvatarURL({ dynamic: true })}`);
+				return interaction.reply(`Your avatar: ${interaction.user.displayAvatarURL({ dynamic: true })}`);
+			}
+			else if (member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES, false)) {
+				interaction.reply('**Manage Messages Permission Required.**');
+			} 
+		}
 	},
 };
+
